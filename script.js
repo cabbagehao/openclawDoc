cbd0a9ffbb9b642bd54dcba2e5fe0847a6c08d27
cabbagehao <cabbagehao@qq.com>
2026-03-13 23:18:08 +0800

    feat: use script.js in both root and docs folder for maximum compatibility

---
+++ script.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4540467205535241";
+  script.setAttribute('crossorigin', 'anonymous');
+  document.head.appendChild(script);
+})();
