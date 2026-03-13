676b028e4c015fbb2c44c9eef4544724017ebb44
cabbagehao <cabbagehao@qq.com>
2026-03-13 22:56:35 +0800

    refactor: use official scripts array for adsense integration

--- docs/adsense.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4540467205535241";
-  script.crossOrigin = "anonymous";
-  document.head.appendChild(script);
-})();
